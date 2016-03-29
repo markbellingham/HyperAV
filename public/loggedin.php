85a223a50e04b8cb5a8127477d21d191d5ee63a3
Mark Bellingham <14032098@stu.mmu.ac.uk>
2016-03-29 14:18:35 +0100

    added Main div to loggedin.php

--- public/loggedin.php
+++ public/loggedin.php
@@ -5,13 +5,19 @@ session_start();
 if (!isset($_SESSION['email'])) {
 	redirect_to("index.php");
 	exit(); 
-}
+} ?>
 
+<div id="main">
+
+<?php
 $page_title = 'Logged In';
 include ('../includes/layouts/header.php');
 
 echo "<h3>Successfully logged in.</h3><p>Welcome  " . $_SESSION['first_name'] . "  , you are now logged in.</p><p><a href=\"logout.php\">Logout</a></p>";
+?>
+</div> <!--ends main -->
 
+<?php
 include ('../includes/layouts/footer.php');
 
 ?> 

3e68231ef52bfeb909327bde5ffaf2e6c2d1eaf8
Mark Bellingham <14032098@stu.mmu.ac.uk>
2016-04-07 11:01:26 +0100

    show previous supplier orders can be filtered by supplier

--- public/prevSupplierOrders.php
+++ public/prevSupplierOrders.php
@@ -10,20 +10,64 @@
 		redirect_to("index.php");
 	}
 
+	if (isset($_GET['supplier'])) {
+		$supplier = $_GET['supplier'];
+	} else {
+		$supplier = "";
+	}
+?>
 
-$query = 'SELECT suName, pr.prModelNo, prName, prPrice, stOrderDate, stDeliveryDate, stOrderQuantity
-		FROM hyperav_supplier su
-		JOIN hyperav_stockorderdetails sod ON su.supplierID = sod.supplierID
-		JOIN hyperav_stock st ON st.stockID = sod.stockID
-		JOIN hyperav_products pr ON pr.prModelNo = st.prModelNo
-		ORDER BY stOrderDate DESC, suName';
+<!-- Populates the drop-down box where the staff member can select from the different suppliers
+	if one of the suppliers is selected, it reloads the page with the selected option as a GET request
+	so that the page only shows the selected supplier  -->
+<?php 
+	$query1 = "SELECT DISTINCT suName FROM hyperav_supplier ORDER BY suName ASC";
+	$results1 = @mysqli_query($connection, $query1);
+	$num_rows1 = mysqli_num_rows($results1);
+	if($results1) {
+		if($num_rows1 > 0) { ?>
+		<form action="prevSupplierOrders.php" method="GET">
+			<select name="supplier" onchange="this.form.submit()">
+				<option>Select Supplier</option>
+				<?php while ($option = mysqli_fetch_array($results1, MYSQLI_ASSOC)) {
+					if ($option['suName'] === $supplier) { ?>
+						<option selected><?php echo $option['suName']; ?></option><?php
+					} else { ?>
+						<option><?php echo $option['suName']; ?></option><?php
+					} 
+				} ?>
+			</select>
+		</form><?php
+		}
+	}
+?>
+
+<?php
+if ($supplier != "" && $supplier != "Select Supplier") {
+	$query = 'SELECT suName, pr.prModelNo, prName, prPrice, stOrderDate, stDeliveryDate, stOrderQuantity
+			FROM hyperav_supplier su
+			JOIN hyperav_stockorderdetails sod ON su.supplierID = sod.supplierID
+			JOIN hyperav_stock st ON st.stockID = sod.stockID
+			JOIN hyperav_products pr ON pr.prModelNo = st.prModelNo 
+			WHERE suName = "' . $supplier . '" 
+			ORDER BY stOrderDate DESC, suName';
+} else {
+	$query = 'SELECT suName, pr.prModelNo, prName, prPrice, stOrderDate, stDeliveryDate, stOrderQuantity
+			FROM hyperav_supplier su
+			JOIN hyperav_stockorderdetails sod ON su.supplierID = sod.supplierID
+			JOIN hyperav_stock st ON st.stockID = sod.stockID
+			JOIN hyperav_products pr ON pr.prModelNo = st.prModelNo
+			ORDER BY stOrderDate DESC, suName';
+}
+
+// print_r($query);
 
 $results = @mysqli_query($connection, $query);
 $num_rows = mysqli_num_rows($results);
 
 if ($results) {
 	if ($num_rows > 0) {
-		$i = "";
+		$i = ""; $grandTotal = 0; // Initialise variables
 		while ($row = mysqli_fetch_array($results, MYSQLI_ASSOC)) {
 			$totalPerItem = $row['prPrice'] * $row['stOrderQuantity'];
 			$grandTotal += $totalPerItem;
@@ -32,9 +76,10 @@ if ($results) {
 			if ($i != $row['stOrderDate']) {
 				echo '</table><br/><br/>';
 
-				echo '<table border="1"><tr><td>Order Date: ' . $row['stOrderDate'] . '</td><td>Delivery Date: ' . $row['stDeliveryDate'] . '</td><td>Total: ' . number_format($grandTotal, 2) . '</td></tr></table>';
+				echo '<table border="1"><tr><td>Order Date: ' . $row['stOrderDate'] . '</td><td>Delivery Date: ' . $row['stDeliveryDate'] . '</td><td>Total: £' . number_format($grandTotal, 2) . '</td></tr></table>';
 
 				echo '<table style="border: 1px solid black"><tr style="font-weight: bold"><td>Supplier Name</td><td>Product Name</td><td>Price</td><td>Quantity</td><td>Total Per Item</td></tr>';
+				$grandTotal = 0; // reset grandTotal
 			}
 
 			echo '<tr><td>' . $row['suName'] . '</td>
@@ -51,6 +96,7 @@ if ($results) {
 }
 
 mysqli_free_result($results);
+mysqli_free_result($results1);
 mysqli_close($connection);
 ?>
 

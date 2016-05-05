50880ce61697d30aa05014ae8b1aa9071cd519e4
Mark Bellingham <14032098@stu.mmu.ac.uk>
2016-05-05 16:58:59 +0100

    convert sql query to prepared statement

--- public/updateCustomer.php
+++ public/updateCustomer.php
@@ -10,7 +10,8 @@
 	redirect_to("index.php");
 }
 
-	$id				= $_POST['customerID'];
+	$id				= (int)$_POST['customerID'];
+	$fname			= $_POST['cuFName'];
 	$lname 			= $_POST['cuLName'];
 	$address1 		= $_POST['address1'];
 	$address2 		= $_POST['address2'];
@@ -21,54 +22,29 @@
 
 	$page_title = 'Customer edited! | HyperAV';
 	
-	 echo '<p>' . $id . '</p>';
-	 echo '<p>' . $lname . '</p>';
-	 echo '<p>' . $address1 . '</p>';
-	 echo '<p>' . $address2 . '</p>';
-	 echo '<p>' . $town . '</p>';
-	 echo '<p>' . $postcode . '</p>';
-	 echo '<p>' . $telephone . '</p>';
-	 echo '<p>' . $cuEmail . '</p>';
+	 // echo '<p>' . $id . ' ' . gettype($id) . '</p>';
+	 // echo '<p>' . $lname . '</p>';
+	 // echo '<p>' . $address1 . '</p>';
+	 // echo '<p>' . $address2 . '</p>';
+	 // echo '<p>' . $town . '</p>';
+	 // echo '<p>' . $postcode . '</p>';
+	 // echo '<p>' . $telephone . '</p>';
+	 // echo '<p>' . $cuEmail . '</p>';
 	
-	// $query = 'UPDATE hyperav_customer SET cuLName ="' . $lname . '", cuAddress1 = "' . $address1 . '", cuAddress2 = "' . $address2 . '", cuTown = "' . $town . '", cuPostcode = "' . $postcode . '", cuTelephone = "' . $telephone . '" WHERE customerID = ' . $id;
-	// $results = @mysqli_query($connection, $query);
-	// $num_rows = mysqli_affected_rows($connection);
-
-	// //echo '<p>' . $query . '</p>';
-
-	// if ($results) {
-	// 	if($num_rows > 0) 
-	// 	{
-	// 		echo '<p>customer information for ' . $lname . ' has been updated</p>';
-	// 		$_SESSION['message'] = 'edited';
-	// 		//header ('Location: selected_product.php?prModelNo=' . $modelNo);
-	// 	} 
-	// 	else 
-	// 	{
-	// 		echo '<p>Customer information for ' . $lname . ' could not be updated</p>';
-	// 		echo '<p>' . mysqli_error($connection) . '</p>';
-	// 	}
-	// } 
-	// else 
-	// {
-	// 	echo '<p>There was an error with the database</p>';
-	// 	echo '<p>' . mysqli_error($connection) . '</p>';
-	// }
-
-	// mysqli_free_result($results);
-
-
-	$query = mysqli_prepare($connection, 'UPDATE hyperav_customer SET cuLName = ?, cuAddress1 = ?, cuAddress2 = ?, cuTown = ?, cuPostcode = ?, cuTelephone = ? WHERE customerID =?');
+	// Create the query statement using prepared statement
+	$query = mysqli_prepare($connection, 'UPDATE hyperav_customer SET cuFName = ?, cuLName = ?, cuAddress1 = ?, cuAddress2 = ?, cuTown = ?, cuPostcode = ?, cuTelephone = ?, cuEmail = ? WHERE customerID = ?');
 	if ($query === false) { trigger_error('Statement failed! ' . htmlspecialchars(mysqli_error($connection)), E_USER_ERROR); }
 
-	$bind = mysqli_stmt_bind_param($query, "ssssssi", $lname, $address1, $address2, $town, $postcode, $telephone, $id);
+	// Bind the variables to the query
+	$bind = mysqli_stmt_bind_param($query, "ssssssssi", $fname, $lname, $address1, $address2, $town, $postcode, $telephone, $cuEmail, $id);
 	if ($bind === false) { trigger_error('Binding parameters failed! ' . E_USER_ERROR); }
 
+	// Execute the query. If it succeeds, display a message to the user
 	$exec = mysqli_stmt_execute($query);
 	if ($exec === false) {
 		trigger_error('Statement execute failed! ' . htmlspecialchars(mysqli_stmt_error($query)), E_USER_ERROR);
 	} else {
-		echo '<p>customer information for ' . $lname . ' has been updated</p>';
+		echo '<p>customer information for ' . $fname . ' ' . $lname . ' has been updated</p>';
 		$_SESSION['message'] = 'edited';
 	}
 

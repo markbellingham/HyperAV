1cd706f741414e319e7ad7a0688374dcf1786f2f
Mark Bellingham <mark.bellingham@protonmail.ch>
2017-06-16 07:47:25 +0800

    added function to get POST value or redirect

--- includes/functions.php
+++ includes/functions.php
@@ -10,8 +10,17 @@
 
 	function get_SESSION_value_or_redirect($key, $page) {
 		if (isset($_SESSION[$key])) {
-			$cart = $_SESSION[$key];
-			return $cart;
+			$session_value = $_SESSION[$key];
+			return $session_value;
+		} else {
+			redirect_to($page);
+		}
+	}
+
+	function get_POST_value_or_redirect($key, $page) {
+		if (isset($_POST[$key])) {
+			$post_value = $_POST[$key];
+			return $post_value;
 		} else {
 			redirect_to($page);
 		}
